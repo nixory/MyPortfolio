e63d3892fc5d43fdd3bae39a07d5d722022ec5a8
nixory <nikitastepanovs777@gmail.com>
2022-09-04 01:21:51 +0300

    portfolio section started

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,2 +1,17 @@
 // Custom Scripts
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
